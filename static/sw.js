96737c72fd37324ffec1a7563ae38b900fa15c60
Alexander Ramirez <alexander_ramirez_c@hotmail.com>
2026-04-23 15:59:42 -0500

    Implement PWA support and update app icons

---
+++ static/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'rct-app-v1';
+const ASSETS = [
+  '/',
+  '/static/css/styles.css',
+  '/static/img/logo.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
